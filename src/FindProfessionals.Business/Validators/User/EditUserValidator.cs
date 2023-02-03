ac452c204ee4129a82d770767626610d8a786ee9
Marcos Almeida <maarcos.almeida@hotmail.com>
2023-02-02 22:33:09 -0300

    Fix password strength in EditUserValidator

--- src/FindProfessionals.Business/Validators/User/EditUserValidator.cs
+++ src/FindProfessionals.Business/Validators/User/EditUserValidator.cs
@@ -33,8 +33,9 @@ namespace FindProfessionals.Business.Validators.User
                 .MinimumLength(8).WithMessage("Your password length must be at least 8.")
                 .MaximumLength(16).WithMessage("Your password length must not exceed 16.")
                 .Matches(@"[a-z]+").WithMessage("Your password must contain at least one lowercase letter.")
+                .Matches(@"[A-Z]+").WithMessage("Your password must contain at least one uppercase letter.")
                 .Matches(@"[0-9]+").WithMessage("Your password must contain at least one number.")
-                .Matches(@"[\!\?\*\.]*$").WithMessage("Your password must contain at least one (!? *.).");
+                .Matches(@"(?=.*?[#?!@$%^&*-])").WithMessage("Your password must contain at least one special character.");
 
             RuleFor(x => x.ConfirmPassword)
                 .Equal(x => x.Password)
